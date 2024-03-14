29a3f3e7232a9e34d6b20c4f324a49633405379c
unknown <sevaram>
2024-03-14 17:46:37 +0530

    the new

---
+++ index14.js
@@ -0,0 +1,27 @@
+// var a=5;
+// var b= a++;
+// console.log(a); =6
+// console.log(b); =5
+
+// var a=2;
+// var b=a++;
+// let c=a+5
+// console.log(a); =3
+// console.log(b); =2
+// console.log(c); =8
+
+// var x=5;
+// console.log(x++); =5
+// console.log(x);   =6
+ 
+// var a=5;
+// var b=a++;
+// var c=b++;
+// var d=c++;
+// console.log(a+b+c+d) =23
+
+// let x=10;
+// let y=x++;
+// let z=y++;
+// let a=z++;
+// console.log(x+y+z+a) =43
